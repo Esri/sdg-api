c59fae97629c34d8767c3aa76643b9a3103bbc03
Adam Pfister <apfister@esri.com>
2016-01-21 13:49:24 -0600

    update

--- models/dashboards.js
+++ models/dashboards.js
@@ -1,17 +1,34 @@
 exports.get = function (query, cb) {
   var data = [];
+  var meta = {};
 
-  if (query.country_code) {
-    data = DASHBOARDS.filter(function (d) {
-      return query.country_code === d.country_code;
-    })
-      [0]
-      .dashboards;
+  if (!query.country_code) {
+    // set to global
+    query.country_code = "GLOBAL";
   }
+
+  if (query.goal) {
+    var country = DASHBOARDS.filter(function (d) {
+      return query.country_code === d.country_code;
+    });
+    
+    if (country && country[0]) {
+      country = country[0];
+      meta.country_name = country.country_name;
+      
+      var d = country.dashboards.filter(function (cd) {
+        return cd.hasOwnProperty(query.goal);
+      });
+
+      if (d && d[0]) {
+        data = d[0][query.goal];
+      }
+    }
+  } 
 
   var json = {
     data: data,
-    meta: {},
+    meta: meta,
     errors: {}
   };
 
